8a6cccba8a52035cc7af6430669d88011375f293
cesteban29 <85302893+cesteban29@users.noreply.github.com>
2022-09-13 09:53:14 -0500

    added ec2 module to main from PMR

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+module "ec2_instance" {
+  source  = "app.terraform.io/cesteban-demos/ec2-instance/aws"
+  version = "= 1.0.0"
 
+  name = "single-instance"
+
+  #ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  #key_name               = "user1"
+  monitoring             = true
+  #vpc_security_group_ids = ["sg-12345678"]
+  #subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
