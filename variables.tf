11fa099b42db8e503bc9fb58c091bbed5938a94e
cesteban29 <85302893+cesteban29@users.noreply.github.com>
2022-11-03 16:06:12 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,5 @@
-variable "access_key" {
-  description = "the access key for accessing AWS"
-}
 
-variable "secret_key" {
-  description = "the secret key for accessing AWS"
-}
-
-variable "instance_size" {
+variable "instance_type" {
   description = "the size of the ec2 instance that you are provisioning"
 }
 
