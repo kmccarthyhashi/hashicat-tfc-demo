43487fcbe01dd7b13cbc2c1b01321e9d5c446b26
cesteban29 <cesteban@hashicorp.com>
2022-11-08 11:24:15 -0600

    added file provisioner

--- main.tf
+++ main.tf
@@ -13,8 +13,28 @@ provider "aws" {
 
 module "hashicat" {
   source  = "app.terraform.io/cesteban-demos/hashicat/aws"
-  version = "1.5.0"
+  version = "1.8.0"
   instance_type = var.instance_type
   region = var.region
 
+}
+
+resource "null_resource" "configure-cat-app" {
+  depends_on = [module.hashicat.aws_eip_association.hashicat]
+
+  triggers = {
+    build_number = timestamp()
+  }
+
+  provisioner "file" {
+    source      = "files/deploy_app.sh"
+    destination = "/home/ubuntu/"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = module.hashicat.tls_private_key.hashicat.private_key_pem
+      host        = module.hashicat.aws_eip.hashicat.public_ip
+    }
+  }
 }
