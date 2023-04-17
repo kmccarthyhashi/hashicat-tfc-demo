3936fda68ca02be4e430d1a99f434388b265aa72
cesteban29 <cesteban@hashicorp.com>
2023-04-17 11:08:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 
 variable "instance_type" {
-  description = "the size of the ec2 instance that you are provisioning"
+  description = "the size of the ec2 instance that you are provisioning!"
 }
 
 variable "region" {
