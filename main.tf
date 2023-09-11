b1cba284d42dde3f5c1659aa69cfd054d3ee7a54
cesteban29 <cesteban@hashicorp.com>
2023-09-11 13:57:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,3 +50,16 @@ check "health_check" {
     error_message = "${data.http.hashicat_web.url} returned an unhealthy status code"
   }
 }
+
+check "ami_version_check" {
+  data "aws_instance" "hashicat_current" {
+    instance_tags = {
+      Name = "demo-hashicat-instance"
+    }
+  }
+
+  assert {
+    condition = data.aws_instance.hashicat_current.ami == data.hcp_packer_image.ubuntu_us_east_1.cloud_image_id
+    error_message = "Must use the latest available AMI, ${data.hcp_packer_image.ubuntu_us_east_1.cloud_image_id}."
+  }
+}
